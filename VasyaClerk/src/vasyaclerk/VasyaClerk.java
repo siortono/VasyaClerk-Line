945dcb6f76b1c76c6179aed8246f7950f7248d44
Mattia Tonolini <51443503+siortono@users.noreply.github.com>
2019-06-06 14:11:06 +0200

    Add files via upload

--- VasyaClerk/src/vasyaclerk/VasyaClerk.java
+++ VasyaClerk/src/vasyaclerk/VasyaClerk.java
@@ -13,9 +13,51 @@ public class VasyaClerk {
 
     /**
      * @param args the command line arguments
+     * @return 
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static String main(String[] args) {
+ 
+   int[] queue = {25, 25, 50}; //Array di prova
+   int money25 = 0;
+   int money50= 0;
+   int money100 = 0;
+
+   for(int i = 0; i < queue.length; i++){
+
+      //Controllo 25
+      if(queue[i] == 25){
+      money25++;
+      }
+
+      //Controllo 50
+      if(queue[i] == 50){
+        if(money25 >= 1){
+            money25--;
+            money50++;
+      } else {
+       return "NO";
+      }
     }
-    
+
+      //Controllo 100
+      if(queue[i] == 100){
+      if(money25 >= 1 && money50 >= 1){
+       money25 -= 1;
+       money50 -= 1;
+       money100++;
+      }else if(money25 >= 3){
+        money25 -= 3;
+        money100++;
+      }else{
+        return "NO";
+      }
+    }
+
+
+  }
+  return "YES";
 }
+}
+
+
+
